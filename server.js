e81383af0a9553b72f2951f39cbfe7fdbdc370bf
love.to.code108 <love.to.code108@gmail.com>
2024-03-27 15:14:42 +0530

    learning how to do jwt token

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+
+
 
 
 const posts = [{
@@ -18,21 +23,74 @@ app.use(express.json());
 
 
 
-app.get('/posts' , (req,res) => {
 
-    res.json(posts);
+
+
+
+
+const authenticateToken = (req,res,next) => {
+
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null) {return res.sendStatus(401);}
+
+
+
+    jwt.verify(token, process.env.JWT_TOKEN_SECRET , (err , user) => {
+        if(err) 
+        {return res.sendStatus(403);}
+        req.user = user
+        next()
+    })
+    
+
+}
+
+
+
+
+app.get('/posts' ,authenticateToken, (req,res) => {
+
+    res.json(posts.filter( post => post.username === req.user.name));
 })
+
+
+
+
+
 
 
 app.post('/login' , (req,res) => {
     // AUTHENTICATE THE USER
 
     const username = req.body.username ;
+    const user = { name : username };
 
-    // fits parameter PAYLOAD , 
-    jwt.sign()
+    // fits parameter PAYLOAD , SECRET_KEY 
+    const acessToken = jwt.sign(user , process.env.JWT_TOKEN_SECRET);
+    res.json({ accessToken : acessToken });
 
 })
+
+
+
+
+
+
+app.get('/', (req, res) => {
+
+    // console.log('GET request received',req.url);
+    console.log(req.ip);
+    res.send("Hello world")
+  });
+
+
+// app.use((req,res) => {
+//     console.log("sex")
+// })
+
+
+
 
 
 app.listen(3000, () => {
